5b5aaf0d26b120a3b0f32154a9069eb83cb89a38
anishparulekar972 <73423751+anishparulekar972@users.noreply.github.com>
2025-02-07 18:02:15 -0800

    Initial Commit

---
+++ formvalidation.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title> Validating Form Data</title>
+</head>
+<body style="background-color:yellow;">
+<p>
+Anish Parulekar - 19203A0048 <br><br>
+</p>
+	<form method="post" action="validdata.php">
+		Name :<input type="text" name="name" id="name" /><br/>
+		Mobile Number :<input type="text" name="mobileno" id="mobileno" /><br/>
+		Email ID :<input type="text" name="email" id="email" /><br/>
+		<input type="submit" name="submit_btn" value="Submit" />
+	</form>
+</body>
+</html>
